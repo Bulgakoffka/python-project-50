88bc709e8e3be2ba151fbe167d4cba6b012e8515
Stephen <nikita2014.zaharov@gmail.com>
2025-06-30 18:19:07 +0400

    refactor: moved test data to special folder, rename main to gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,15 +1,21 @@
 import argparse as ap
+# позже посмотреть требования проекта к названиям файлов. если нет, то переименовать в main
 
-
-def main():
+def parser_function():
     parser = ap.ArgumentParser(
                     prog='gendiff',
                     description='Compares two configuration files and shows a difference.',
                     usage='gendiff [-h] first_file second_file'
     )
+    parser.add_argument('-f', '--format', help='set format of output')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.parse_args()
+    return parser.parse_args()
+
+def main():
+    args = parser_function()
+    first_file = args.first_file
+    second_file = args.second_file
 
 if __name__ == '__main__':
-    main()
+    ...
