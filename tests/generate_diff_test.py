2087376be5ae466dcd7749b677e55a9a8d29380d
Stephen <146680209+Bulgakoffka@users.noreply.github.com>
2025-07-08 02:13:05 +0400

    feat: create tests for generate diff

---
+++ tests/generate_diff_test.py
@@ -0,0 +1,16 @@
+import pytest
+from pathlib import Path
+from gendiff.scripts.generate_diff import generate_diff
+
+def test_generate_diff():
+    base_path = Path(__file__).resolve().parent.parent
+    file1 = base_path / "tests" / "test_data" / "file1.json"
+    file2 = base_path / "tests" / "test_data" / "file2.json"
+    assert generate_diff(file1, file2) == '''{
+- follow: False
+  host: hexlet.io
+- proxy: 123.234.53.22
+- timeout: 50
++ timeout: 20
++ verbose: True
+}'''
