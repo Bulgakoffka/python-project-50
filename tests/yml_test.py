e1ed29849b82ff6ad0b0aa56d2fd7c4c43039055
Stephen <146680209+Bulgakoffka@users.noreply.github.com>
2025-07-12 00:06:20 +0400

    Add YAML diff test and test data files
    Introduces yml_test.py to test generate_diff with YAML files and adds sample YAML files for testing. Also imports pytest in generate_diff_test.py to support testing.

--- tests/yml_test.py
+++ tests/yml_test.py
@@ -5,8 +5,8 @@ from gendiff.modules.generate_diff import generate_diff
 
 def test_generate_diff():
     base_path = Path(__file__).resolve().parent.parent
-    file1 = base_path / "tests" / "test_data" / "file1.json"
-    file2 = base_path / "tests" / "test_data" / "file2.json"
+    file1 = base_path / "tests" / "test_data" / "file1.yaml"
+    file2 = base_path / "tests" / "test_data" / "file2.yaml"
     assert generate_diff(file1, file2) == '''{
 - follow: False
   host: hexlet.io
