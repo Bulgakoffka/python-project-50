2b93eed5511eac960e9a49f0f4cc34ce1d4e5d24
Stephen <146680209+Bulgakoffka@users.noreply.github.com>
2025-11-13 01:09:56 +0400

    transferring default format option into parser

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -30,5 +30,5 @@ def arg_parser():  # pragma: no cover
     parser.add_argument("-f", "--format", help="set format of output")
     parser.add_argument("first_file")
     parser.add_argument("second_file")
-    parser.add_argument('format_name', nargs='?')
+    parser.add_argument('format_name', nargs='?', default='stylish')
     return parser.parse_args()
