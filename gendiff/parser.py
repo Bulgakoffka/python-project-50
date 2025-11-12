b92c6b8cacdff71a9b2beb0d4df670e882bc0b99
Stephen <146680209+Bulgakoffka@users.noreply.github.com>
2025-11-13 01:02:43 +0400

    fix: delete default value from parser

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -30,5 +30,5 @@ def arg_parser():  # pragma: no cover
     parser.add_argument("-f", "--format", help="set format of output")
     parser.add_argument("first_file")
     parser.add_argument("second_file")
-    parser.add_argument('format_name', nargs='?', default='stylish')
+    parser.add_argument('format_name', nargs='?')
     return parser.parse_args()
